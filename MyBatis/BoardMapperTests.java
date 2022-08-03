ced9b5cc6751db98adb46d4daa64ecefa08efebe
eunhye22 <leh5089@hanmail.net>
2022-08-03 21:53:21 +0900

    BoardMapperTests

--- MyBatis/BoardMapperTests.java
+++ MyBatis/BoardMapperTests.java
@@ -1,7 +1,5 @@
 package org.zerock.myapp.persistence;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -65,10 +63,41 @@ public class BoardMapperTests {
 		SqlSession sqlSession = this.sqlSessionFactory.openSession();
 		
 		try (sqlSession) {
-			String namespace = "Choonsik";
-			String sqlId = "selectAllBoards";
+			// 마이바티스는 아래와 같이 2개의 값을 이용해서, 다수의 Mapper XML 파일이 있을 때
+			// (1) 어느 특정 Mapper XML을 사용할지 결정하고 (by "namespace")
+			// (2) 다시 결정된 Mapper XML 파일 안에 있는 많은 SQL 태그 중에,
+			//     어느 태그의 SQL 문장을 사용할지 결정 (by "sqlID")
 			
-			String sql = namespace + "." + sqlId;	// Mapped Statement
+			String namespace = "Choonsik";		// Mapper XML 파일마다 지정된 이름
+			String sqlId = "selectAllBoards";	// 특정 Mapper XML파일 안에 있는 특정 SQL태그의 식별자
+			
+			String sql = namespace + "." + sqlId;	// Mapped Statement -> 최종 수행시킬 SQL문장의 식별자값
+			
+			
+			@Cleanup("clear")	// list.clear();
+			List<BoardVO> list = sqlSession.<BoardVO>selectList(sql);
+		
+			list.forEach(log::info);
+		} // try-with-resources
+	}// testSelectAllBoards
+	
+	@Test
+	@Order(3)
+	@DisplayName("3.testSelectBoards")
+	@Timeout(value=30, unit=TimeUnit.SECONDS)
+	void testSelectBoards() {
+		log.trace("testSelectBoards() invoked.");
+		
+		@Cleanup
+		SqlSession sqlSession = this.sqlSessionFactory.openSession();
+		
+		try (sqlSession) {
+			
+			String namespace = "Choonsik";		// Mapper XML 파일마다 지정된 이름
+			String sqlId = "selectAllBoards";	// 특정 Mapper XML파일 안에 있는 특정 SQL태그의 식별자
+			
+			String sql = namespace + "." + sqlId;	// Mapped Statement -> 최종 수행시킬 SQL문장의 식별자값
+			
 			
 			@Cleanup("clear")	// list.clear();
 			List<BoardVO> list = sqlSession.<BoardVO>selectList(sql);
